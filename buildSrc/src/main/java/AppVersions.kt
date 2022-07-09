7a5306a7f37ee4cf83f00a2175aa2a2590ab961e
Hideharu Nagakura <nagakura@fout.jp>
2022-07-09 21:50:29 +0900

    Initial commit

---
+++ buildSrc/src/main/java/AppVersions.kt
@@ -0,0 +1,11 @@
+package com.example.apps.battery
+
+object AppVersions {
+    const val versionName = "1.0.0" // X.Y.Z; X = Major, Y = minor, Z = Patch level
+    const val versionCode = 1
+
+    const val compile_sdk = 31
+    const val build_tools = "29.0.3"
+    const val min_sdk = 23
+    const val target_sdk = 31
+}
---
+++ buildSrc/src/main/java/Deps.kt
@@ -0,0 +1,17 @@
+package com.example.apps.battery
+
+object Deps {
+    const val android_gradle_plugin = "com.android.tools.build:gradle:4.1.3"
+    const val appcompat = "androidx.appcompat:appcompat:1.4.2"
+    const val constraint_layout = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val core_ktx = "androidx.core:core-ktx:1.8.0"
+    const val material = "com.google.android.material:material:1.6.0"
+    const val espresso_core = "androidx.test.espresso:espresso-core:3.4.0"
+    const val ext_junit = "androidx.test.ext:junit:1.1.4"
+    const val junit = "junit:junit:4.12"
+
+    object Kotlin {
+        const val plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0"
+        const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib:1.6.0"
+    }
+}
